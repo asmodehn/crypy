49b689eed48e5c9ac9c184305c9ac410820a0d6a
AlexV <asmodehn@gmail.com>
2019-01-08 16:58:27 +0100

    Added spyder as dependency, along with cryptocompare and small test script.

---
+++ crypy/graph/__init__.py
@@ -0,0 +1,2 @@
+# Provide a way to visualiez relationships between currency/pairs etc.
+# TODO : check networkx
--- crypy/term/__init__.py
+++ crypy/term/__init__.py
@@ -1,3 +1,4 @@
 """
 Provide a way to visualize supervisors and traders data, all in a terminal (possibly via unicode encodings for greater details)
+TODO : Check chafa
 """
