37eefe0744c084f3c8246428f38b7a66ff943e0c
AlexV <asmodehn@gmail.com>
2019-01-24 18:18:45 +0100

    adding cli interface for querying exchanges.

---
+++ crypy/exchange/__main__.py
@@ -0,0 +1,74 @@
+# Running an excahnge in one process from command line
+# Note the process interface is more complex to use, but more reliable.
+# Goal is to provide a process interface (supporting death and rebirth)
+# and internally using a usual python API.
+# this __main__ file should provide all that's needed for the process interface to be used.
+
+import click
+
+
+import crypy.config
+import crypy.pair
+import crypy.euc
+
+
+aliases = {
+    'kraken.com': ['kraken', ],
+    'testnet.bitmex.com': ['testnet', 'testnet-bitmex'],  #TODO ccxt test ?
+    'bitmex.com': ['bitmex', ],
+}
+
+implementations = {
+    'kraken.com': lambda exc: crypy.euc.ccxt.kraken(exc),
+    'testnet.bitmex.com': lambda exc: crypy.euc.ccxt.bitmex(exc), #TODO ccxt test ?
+    'bitmex.com': lambda exc: crypy.euc.ccxt.bitmex(exc),
+}
+
+# Making sure we dont miss anything here
+assert aliases.keys() == implementations.keys()
+
+
+@click.group(name='exchange', invoke_without_command=True)
+@click.option('-e', '--exchange', type=click.Choice(['kraken', 'testnet-bitmex', 'bitmex']), default='kraken')
+@click.option('-v', '--verbose', is_flag=True, default=False)
+@click.pass_context
+def exchange_cli(ctx, exchange=None, verbose=False):
+
+    exchange = 'kraken' if exchange is None else exchange
+
+    # finding the actual config name for this alias
+    for e, a in aliases.items():
+        if exchange in a:
+            exchange = e
+            break
+
+    config = dict(crypy.config.config().items(exchange))
+
+    config['verbose'] = verbose
+
+    ctx.obj = {
+        'exchange': implementations[exchange](config),
+    }
+
+    if ctx.invoked_subcommand is None:
+        # default command
+        ctx.invoke(markets)
+    # otherwise invoke the specified subcommand (default behavior)
+
+
+@exchange_cli.command(name='tradable')
+# We can show one or more, default to graph.
+#TODO : more views
+@click.option('-v', '--verbose', is_flag=True, default=False)
+@click.pass_obj
+def markets(obj, verbose=False):
+
+    from pprint import pprint
+
+    markets = {d.get('id'): d for d in obj.get('exchange').fetch_markets() if d.get('active')}
+
+    pprint(markets)
+
+
+if __name__ == '__main__':
+    exchange_cli()
