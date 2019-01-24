e69c95ac53ac07ea0d7526aa4bd5e69513b758b6
AlexV <asmodehn@gmail.com>
2019-01-24 14:13:49 +0100

    adding terminal graph for krk script.

--- krk.py
+++ krk.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 # import krakenex
 # from pykrakenapi import KrakenAPI
 # api = krakenex.API()
@@ -7,6 +9,8 @@
 
 import crypy.euc
 ccxt = crypy.euc.ccxt
+
+from crypy.chart import plot
 
 import configparser
 config = configparser.ConfigParser()
@@ -19,3 +23,30 @@ kraken = ccxt.kraken(dict(config.items('kraken.com')))
 print(kraken.fetch_balance())
 print(kraken.fetch_trading_fees())
 print(kraken.fetch_ticker('BTC/USD'))
+
+
+# each ohlcv candle is a list of [ timestamp, open, high, low, close, volume ]
+index = 4  # use close price from each ohlcv candle
+
+
+def print_chart(exchange, symbol, timeframe):
+
+    print("\n" + exchange.name + ' ' + symbol + ' ' + timeframe + ' chart:')
+
+    # get a list of ohlcv candles
+    ohlcv = exchange.fetch_ohlcv(symbol, timeframe)
+
+    # get the ohlCv (closing price, index == 4)
+    series = [x[index] for x in ohlcv]
+
+    # print the chart
+    print("\n" + plot(series[-120:], {'height': 20}))  # print the chart
+
+    last = ohlcv[len(ohlcv) - 1][index]  # last closing price
+    return last
+
+
+last = print_chart(kraken, 'BTC/USD', '1h')
+print("\n" + kraken.name + " ₿ = $" + str(last) + "\n")  # print last closing price
+
+
