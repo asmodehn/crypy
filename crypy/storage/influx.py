936cb39bcd93225fbadeeba5f6b0542b729c16af
AlexV <asmodehn@gmail.com>
2019-01-29 14:42:46 +0100

    adding influx client tuto.

---
+++ crypy/storage/influx.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""Tutorial for using pandas and the InfluxDB client."""
+
+import argparse
+import pandas as pd
+import numpy as np
+
+from influxdb import DataFrameClient
+
+
+def main(host='localhost', port=8086):
+    """Instantiate the connection to the InfluxDB client."""
+    user = 'root'
+    password = 'root'
+    dbname = 'crypytest'
+    # Temporarily avoid line protocol time conversion issues #412, #426, #431.
+    protocol = 'json'
+
+    client = DataFrameClient(host, port, user, password, dbname)
+
+    print("Create pandas DataFrame")
+    df = pd.DataFrame(data=(i for i in np.arange(30)),
+                      index=pd.date_range(start='2019-01-16',
+                                          periods=30, freq='H'), columns=['bouh'])
+
+    print(df)
+
+    print("Create database: " + dbname)
+    client.create_database(dbname)
+
+    print("Write DataFrame")
+    client.write_points(df, 'demo', protocol=protocol)
+
+    print("Write DataFrame with Tags")
+    client.write_points(df, 'demo',
+                        {'k1': 'v1', 'k2': 'v2'}, protocol=protocol)
+
+    #print("Read DataFrame")
+    #client.query("select * from demo")
+
+    #print("Delete database: " + dbname)
+    #client.drop_database(dbname)
+
+
+def parse_args():
+    """Parse the args from main."""
+    parser = argparse.ArgumentParser(
+        description='example code to play with InfluxDB')
+    parser.add_argument('--host', type=str, required=False,
+                        default='localhost',
+                        help='hostname of InfluxDB http API')
+    parser.add_argument('--port', type=int, required=False, default=8086,
+                        help='port of InfluxDB http API')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(host=args.host, port=args.port)
