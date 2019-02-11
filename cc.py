965342b4955f130da6fad1f004fec83922738827
XorfacX <xorfacx@fairydwarves.com>
2019-02-11 18:10:33 +0100

    getAllHisto for default pair/exchange and save it (wip)

--- cc.py
+++ cc.py
@@ -22,38 +22,6 @@ import cryptocompare.cryptocompare as crcomp # https://github.com/lagerfeuer/cry
 #print( crcomp.get_historical_price_minute() )
 
 
-"""
-import matplotlib.pyplot as plt   # https://github.com/matplotlib/matplotlib
-import numpy as np
-
-fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(9, 4))
-
-# generate some random test data
-all_data = [np.random.normal(0, std, 100) for std in range(6, 10)]
-
-# plot violin plot
-axes[0].violinplot(all_data,
-                   showmeans=False,
-                   showmedians=True)
-axes[0].set_title('violin plot')
-
-# plot box plot
-axes[1].boxplot(all_data)
-axes[1].set_title('box plot')
-
-# adding horizontal grid lines
-for ax in axes:
-    ax.yaxis.grid(True)
-    ax.set_xticks([y+1 for y in range(len(all_data))])
-    ax.set_xlabel('xlabel')
-    ax.set_ylabel('ylabel')
-
-# add x-tick labels
-plt.setp(axes, xticks=[y+1 for y in range(len(all_data))],
-         xticklabels=['x1', 'x2', 'x3', 'x4'])
-plt.show()
-"""
-
 
 ################################################################################################
 #	name:	timeseries_OHLC_with_SMA.py
@@ -62,6 +30,8 @@ plt.show()
 #	Author:	conquistadorjd
 #   https://www.techtrekking.com/how-to-plot-simple-and-candlestick-chart-using-python-pandas-matplotlib/
 ################################################################################################
+import time
+
 import pandas
 # import pandas_datareader as datareader
 import matplotlib.pyplot as plt
@@ -73,111 +43,102 @@ import json
 
 import TechnicalIndicators.indicator.indicators as ti
 
-#look into https://plot.ly/python/candlestick-charts/
 
-#df = pandas.read_json(crcomp.get_historical_price_minute())
-#df.locations = pandas.DataFrame(df.locations.values.tolist())['Data']
-#df = df.groupby(['date','name','number'])['locations'].apply(','.join).reset_index()
-
-#print( json.loads( str( crcomp.get_historical_price_minute() ) ) )
-df = pandas.read_json( json.dumps( crcomp.get_historical_price_minute('ETH','EUR','Kraken')['Data'] ) )
-# http://pandas.pydata.org/pandas-docs/stable/generated/pandas.read_json.html
-
-#df = type(crcomp.get_historical_price_minute())
-#print(json.loads(str(crcomp.get_historical_price_minute())))
-#df = pandas.read_json()
+##look into https://plot.ly/python/candlestick-charts/
+#import plotly as py
+#import plotly.graph_objs as go
+#
+#
+#df = pandas.read_json( json.dumps( crcomp.get_historical_price_minute('ETH','USD','Kraken')['Data'] ) )
+## http://pandas.pydata.org/pandas-docs/stable/generated/pandas.read_json.html
+#
+##df = type(crcomp.get_historical_price_minute())
+##print(json.loads(str(crcomp.get_historical_price_minute())))
+##df = pandas.read_json()
+##print(df)
+##time: 1548006900
+##close: 2772.27
+##high: 2772.45
+##low: 2771.81
+##open: 2772.45
+##volumefrom: 0.5832
+##volumeto: 1610.12
+#
+#
+## Converting date to pandas datetime format
+#df['time'] = pandas.to_datetime(df['time'], unit='s')
+#df['time'] = df['time'].apply(mdates.date2num)
 #print(df)
-#time: 1548006900
-#close: 2772.27
-#high: 2772.45
-#low: 2771.81
-#open: 2772.45
-#volumefrom: 0.5832
-#volumeto: 1610.12
-
-#df = pandas.read_csv('15-06-2016-TO-14-06-2018HDFCBANKALLN.csv')
-
-# ensuring only equity series is considered
-#df = df.loc[df['Series'] == 'EQ']
-
-# Converting date to pandas datetime format
-#df['Date'] = pandas.to_datetime(df['Date'])
-#df["Date"] = df["Date"].apply(mdates.date2num)
-df['time'] = pandas.to_datetime(df['time'], unit='s')
-df['time'] = df['time'].apply(mdates.date2num)
-print(df)
-
-# Creating required data in new DataFrame OHLC
-ohlc = df[['time', 'open', 'high', 'low', 'close']].copy()
-print(ohlc)
-
-# Formatting for indicator 'date', 'open', 'high', 'low', 'close', 'volume'
-#ha = df[['open', 'high', 'low', 'close']].copy()
-#ha['date'] = df['time']
-df['volume'] = df['volumeto'] - df['volumefrom']
-ha = ti.HA(df, ['open', 'high', 'low', 'close'])
-ha = ha.drop(['open', 'high', 'low', 'close', 'volumefrom', 'volumeto'], axis=1)
-ha = ha.rename(index=str, columns={"HA_close": "close", "HA_open": "open", "HA_high": "high", "HA_low": "low"})
-print(ha)
-
-# In case you want to check for shorter timespan
-# ohlc =ohlc.tail(60)
-
-"""
-df = pandas.read_csv('15-06-2016-TO-14-06-2018HDFCBANKALLN.csv')
-
-df = df.loc[df['Series'] == 'EQ']
-
-df['Date'] = pandas.to_datetime(df['Date'])
-df["Date"] = df["Date"].apply(mdates.date2num)
-
-
-ohlc= df[['Date', 'Open Price', 'High Price', 'Low Price','Close Price']].copy()
-print(ohlc)
-"""
+#
+## Creating required data in new DataFrame OHLC
+#ohlc = df[['time', 'open', 'high', 'low', 'close']].copy()
+#print(ohlc)
+#
+## Formatting for HA 'date', 'open', 'high', 'low', 'close', 'volume'
+#df['volume'] = df['volumeto'] - df['volumefrom']
+#ha = ti.HA(df, ['open', 'high', 'low', 'close'])
+#ha = ha.drop(['open', 'high', 'low', 'close', 'volumefrom', 'volumeto'], axis=1)
+#ha = ha.rename(index=str, columns={"HA_close": "close", "HA_open": "open", "HA_high": "high", "HA_low": "low"})
+##print(ha)
+#
+## In case we want to check for shorter timespan
+## ohlc = ohlc.tail(60)
+#
+#
+#f1, ax = plt.subplots(figsize = (10,5))
+#
+## plot the candlesticks
+#candlestick_ohlc(ax, ohlc.values, width=.6, colorup='green', colordown='red')
+#ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
+#
+### Creating SMA columns
+#ohlc['SMA50'] = ohlc["close"].rolling(50).mean()
+#ohlc['SMA100'] = ohlc["close"].rolling(100).mean()
+#
+##Plotting SMA columns
+#ax.plot(ohlc['time'], ohlc['SMA50'], color = 'green', label = 'SMA50')
+#ax.plot(ohlc['time'], ohlc['SMA100'], color = 'blue', label = 'SMA100')
+#
+## Saving image
+## plt.savefig('OHLC with SMA HDFC.png')
+## Save file
+#ohlc.to_csv('ohlc.csv')
+##plt.show()
+#
+#
+## plotly TEST
+#trace = go.Candlestick(x=ha['time'],
+#                open=ha['open'],
+#                high=ha['high'],
+#                low=ha['low'],
+#                close=ha['close'])
+#data = [trace]
+#py.offline.plot(data, filename='simple_candlestick.html')
 
 
+def getAllHisto(symbol, currency, exchange, timeframe):
+    #if type(timeframe) is str and timeframe in ['minute', 'hour', 'day'] and type(symbol) is str and type(cyrrency) is str and type(exchange) is str :
+        i = 0, limit = 2000, toTs = 0
+        allDfs = dict()
+        while(i<5):
+            allDfs[i] = pandas.read_json( json.dumps( crcomp.get_historical_price_minute(symbol, currency, exchange, 1, True, 2000)['Data'] ) ) #toTs TODO
+            #print(allDfs[i])
+            
+            i = i+1
+        
+        #print(allDfs) 
+        df = pandas.concat(allDfs)
+        #print(df)
 
-
-f1, ax = plt.subplots(figsize = (10,5))
-
-# plot the candlesticks
-candlestick_ohlc(ax, ohlc.values, width=.6, colorup='green', colordown='red')
-ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
-
-## Creating SMA columns
-#ohlc['SMA5'] = ohlc["close"].rolling(5).mean()
-#ohlc['SMA10'] = ohlc["close"].rolling(10).mean()
-#ohlc['SMA20'] = ohlc["close"].rolling(20).mean()
-ohlc['SMA50'] = ohlc["close"].rolling(50).mean()
-ohlc['SMA100'] = ohlc["close"].rolling(100).mean()
-#ohlc['SMA200'] = ohlc["close"].rolling(200).mean()
-
-#Plotting SMA columns
-# ax.plot(ohlc['time'], ohlc['SMA5'], color = 'blue', label = 'SMA5')
-#ax.plot(ohlc['time'], ohlc['SMA10'], color = 'orange', label = 'SMA10')
-#ax.plot(ohlc['time'], ohlc['SMA20'], color = 'red', label = 'SMA20')
-ax.plot(ohlc['time'], ohlc['SMA50'], color = 'green', label = 'SMA50')
-ax.plot(ohlc['time'], ohlc['SMA100'], color = 'blue', label = 'SMA100')
-#ax.plot(ohlc['time'], ohlc['SMA200'], color = 'blue', label = 'SMA200')
-
-# Saving image
-#plt.savefig('OHLC with SMA HDFC.png')
-
-# In case you dont want to save image but just displya it
-
-# ohlc.to_csv('ohlc.csv')
-#plt.show()
-
-
-import plotly as py
-import plotly.graph_objs as go
-
-trace = go.Candlestick(x=ha['time'],
-                open=ha['open'],
-                high=ha['high'],
-                low=ha['low'],
-                close=ha['close'])
-data = [trace]
-py.offline.plot(data, filename='simple_candlestick.html')
-
+        df['time'] = pandas.to_datetime(df['time'], unit='s')
+        df['time'] = df['time'].apply(mdates.date2num)
+        ohlcv = df[['time', 'open', 'high', 'low', 'close']].copy()
+        ohlcv['volume'] = df['volumeto'] - df['volumefrom']
+        ohlcv.to_csv('OHLCV_'+ symbol + currency + '_' + exchange + '_' + timeframe +'.csv')
+        print ('Done')
+        
+    #else :
+    #    print('invalid param(s) specified')
+    
+    
+getAllHisto('ETH', 'USD', 'kraken', 'minute')
