6a3e915d5dfa3e67d4c9a736bab37345cd29b199
AlexV <asmodehn@gmail.com>
2019-02-11 15:10:58 +0100

    improvements on config

--- crypy/config.py
+++ crypy/config.py
@@ -1,11 +1,13 @@
 import logging
 import os
-from pathlib import Path
+from typing import Optional
+from pathlib import Path, PosixPath
+import configparser
 
-sample_config = """
+sample_config_file = """
 [kraken.com]
-  apiKey = "YOUR API KEY"
-  secret = "YOUR API SECRET"
+  apiKey = "YOUR KRAKEN API KEY"
+  secret = "YOUR KRAKEN API SECRET"
   verbose = true
 
 [testnet.bitmex.com]
@@ -17,20 +19,21 @@ sample_config = """
 
 """
 
-locations = [
-    os.curdir,
-    os.path.expanduser("~"),
-    "/etc/crypy",
-    os.environ.get("CRYPY_CONF")
-]
 
-
-def locate(filename=None):
+def locate(filename: Optional[str] = None) -> Path:
     config_file = None
 
     filename = 'crypy.ini' if filename is None else filename
+    l=[
+        os.curdir,
+        os.path.join(os.getcwd(), os.path.pardir),
+        os.path.expanduser("~"),
+        "/etc/crypy",
+        os.environ.get("CRYPY_CONF")
+    ]
 
-    for loc in locations:
+    # We loop on turthy value only (no empty string, no none, etc.)
+    for loc in (p for p in l if p):
         locfile = Path(loc).joinpath(filename)
         if locfile.exists():
             config_file = locfile
@@ -39,23 +42,36 @@ def locate(filename=None):
     if config_file is None:  # If not present, generate sample file
         config_file = Path(os.curdir).joinpath(filename)
         logging.warning("config file not found. {config_file} has been generated for you.")
-        config_file.write_text(sample_config)
+        config_file.write_text(sample_config_file)
 
     config_file.resolve()
     return config_file
 
 
-def config(filename=None):
+_default_config = {}
+
+_config_instances = dict()
+
+config = _default_config
+
+
+def configure(filename: Optional[str] = None, reset: bool = False) -> configparser.ConfigParser:
 
     config_file = locate(filename)
 
-    # Preparing config
-    import configparser
-    config = configparser.ConfigParser()
-    config.optionxform = str
+    if (config_file not in _config_instances) or reset:
 
-    # Loading file
-    config.read(str(config_file))
+        # Preparing config
+        c = configparser.ConfigParser()
+        c.optionxform = str
+
+        # Loading file
+        c.read(str(config_file))
+
+        _config_instances[config_file] = dict(c)
+
+    # we set the module attribute
+    config = _config_instances.get(config_file, _default_config)
 
     return config
 
---
+++ crypy/tests/test_config.py
@@ -0,0 +1,15 @@
+import pytest
+
+
+def test_locate():
+    pass
+
+def test_config():
+    pass
+
+
+
+
+
+if __name__ == '__main__':
+    pytest.main(['-s', __file__])
