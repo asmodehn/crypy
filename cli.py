2bc0da6f47ac6ef6b8a2a4b20e762b7efebecbbd
AlexV <asmodehn@gmail.com>
2019-02-12 13:14:31 +0100

    quick cli fix

--- cli.py
+++ cli.py
@@ -60,12 +60,69 @@ wholeData = {
                 'datetime': '2018/09/02 15:32:12'
             }
         ]
+    },
+    'ETHEUR': {
+        'data': {
+            'value': 100,
+            'indicators': { 'rsi': 49.01, 'ew': 'five of five of 1 up' },
+            #### TIMEFRAME for data ?? ####
+            'orderbook': []
+        },
+        'positions': [
+            {
+                'side': 'short',
+                'amount': 60,
+                'price': 94.19,
+                'leverage': 5,
+                'p/l %': 10,
+                'p/l': 800
+            }
+        ],
+        'orders': [
+            {
+                'side': 'short',
+                'type': 'limit',
+                'amount': 30,
+                'price': 86,
+                'leverage': 5,
+                'expiracy': '1 hour'
+            },
+            {
+                'side': 'short',
+                'type': 'limit',
+                'amount': 34,
+                'price': 76,
+                'leverage': 10
+            }
+        ],
+        'trades': [
+            {
+                'side': 'short',
+                'type': 'limit',
+                'amount': 35,
+                'price': 53,
+                'leverage': 5,
+                'datetime': '2018/05/02 16:32:12'
+            },
+            {
+                'side': 'long',
+                'type': 'limit',
+                'amount': 35,
+                'price': 89,
+                'leverage': 5,
+                'datetime': '2018/09/02 15:34:12'
+            }
+        ]
     }
 }
 
+
 class StackableCmd(cmd.Cmd):
-    def __init__(self, prompt, completekey='tab', stdin=None, stdout=None):
-        self.prompt = prompt + ">"
+    # Note the cmd.Cmd super class is shared between subclasses : careful managing it's state...
+    def __init__(self, prompt_apd, completekey='tab', stdin=None, stdout=None):
+        # managing class state
+        self._old_prompt = self.prompt
+        self._apd_prompt = prompt_apd
         super().__init__(completekey=completekey, stdin=stdin, stdout=stdout)
 
     def precmd(self, line):
@@ -75,54 +132,63 @@ class StackableCmd(cmd.Cmd):
         return stop
 
     def preloop(self):
-        pass
+        # using loop to easily manage the cmd.Cmd state
+        # TODO : functional design with immutable state for clarity
+        self.prompt = self._old_prompt + self._apd_prompt + ">"
 
     def postloop(self):
-        pass
+        # using loop to easily manage the cmd.Cmd state
+        # TODO : functional design with immutable state for clarity
+        self.prompt = self.prompt.split('>')[:-1]
 
     def do_exit(self, arg):
         return True
 
     def do_EOF(self, arg):
-        # BROKEN : Closes stdin
+        # BROKEN : stdin stays closed ?
         # TODO : fixit
         return True
 
 
-class Trader(StackableCmd):
-    def preloop(self):
-        print("entering position")
-
-    def postloop(self):
-        print("exiting position")
-
-class Holder(StackableCmd):
-
-    def preloop(self):
-        print("managing assets")
-        with open(os.dup(sys.stdin.fileno()), sys.stdin.mode) as stdin:
-            t = Trader(self.prompt + pair, stdin=stdin)
-
-            t.cmdloop(f"Position on {pair}")
+# class Trader(StackableCmd):
+#     def preloop(self):
+#         print("entering position")
+#
+#     def postloop(self):
+#         print("exiting position")
+#
+# class Holder(StackableCmd):
+#
+#     def preloop(self):
+#         print("managing assets")
+#         with open(os.dup(sys.stdin.fileno()), sys.stdin.mode) as stdin:
+#             t = Trader(self.prompt + pair, stdin=stdin)
+#
+#             t.cmdloop(f"Position on {pair}")
 
 
 class Pair(StackableCmd):
-    def __init__(self, prompt, completekey='tab', stdin=None, stdout=None, usedPair = defPAIR):
-        self.usedPair = usedPair
-        print(prompt)
-        super().__init__(prompt, completekey, stdin, stdout)
+    def __init__(self, prompt_apd: str, usedPair: str = None):
+        self.usedPair = defPAIR if usedPair is None else usedPair.upper()
+
+        # minimum of validation
+        if self.usedPair not in wholeData.keys():
+            raise RuntimeError("Unknown Pair")
+
+        super().__init__(prompt_apd=prompt_apd)
 
     def preloop(self):
         print("managing pair " + self.usedPair)
+        super().preloop()
 
     # all commands below are subcommand available when the pair is define above
     def do_list_data(self):
         print(f"displaying {self.usedPair} infos (price, graph, analisys, indicators)")
         print(wholeData[self.usedPair].data)
 
-    def do_list_orders(self):
+    def do_orders(self, arg):
         print(f"displaying {self.usedPair} current orders")
-        print(wholeData[self.usedPair].orders)
+        print(wholeData[self.usedPair].get('orders'))
 
     def do_list_positions(self):
         print(f"displaying {self.usedPair} current positions")
@@ -154,8 +220,15 @@ class Pair(StackableCmd):
         # todo linkable/delinkable to trailing_stop_XXX
 
 
+# Desk is not a stackable command.
+# it is the main entry point to all the other (sub/stackable) commands
+class Desk(cmd.Cmd):
 
-class Desk(StackableCmd):
+    def __init__(self, prompt=None):
+
+        self.prompt = ("desk" if prompt is None else prompt) + ">"
+        super().__init__()
+
     def do_list_positions(self):
         print(f"list all current positions for all pairs")
     def do_list_orders(self):
@@ -163,11 +236,23 @@ class Desk(StackableCmd):
     def do_list_trades(self):
         print(f"list all past trades for all pairs")
 
-    def do_use_pair(self, pair=defPAIR):
-        with open(os.dup(sys.stdin.fileno()), sys.stdin.mode) as stdin:
-            pair = defPAIR if pair is '' else pair.upper()
-            t = Pair(prompt=(self.prompt + pair), stdin=stdin, usedPair=defPAIR)
-            t.cmdloop()
+
+    def do_exit(self, arg):
+        return True
+
+    def do_EOF(self, arg):
+        # BROKEN : stdin stays closed ?
+        # TODO : fixit
+        return True
+
+
+    def do_pair(self, pair="ETHUSD"):
+        """
+        Uses a specific pair. Choose one of [ETHUSD, ETHEUR]
+        """
+        Pair.prompt = self.prompt  # patching class prompt, as beginning of the prompt stack
+        t = Pair(prompt_apd=pair, usedPair=pair)
+        t.cmdloop()
 
 '''
     def do_invest(self, asset="EUR"):
