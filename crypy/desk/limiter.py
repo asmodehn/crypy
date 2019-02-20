bc3400090cc86f8842937d49ce30555b4662e2f0
AlexV <asmodehn@gmail.com>
2019-02-20 11:58:58 +0100

    currently working wait and drop limiter. still untested.

--- crypy/desk/limiter.py
+++ crypy/desk/limiter.py
@@ -1,3 +1,13 @@
+import inspect
+import math
+
+import profile  #: higher overhead and require calibration. BEtter for python debugging, and maybe better on average after calibration ?
+#import cProfile as profile  # better for quick results, after the dev phase.
+import pstats
+
+# TODO : nice output eventually : https://stackoverflow.com/questions/4544784/how-can-you-get-the-call-tree-with-python-profilers
+import sys
+
 import typing
 import functools
 from dataclasses import dataclass, field
@@ -18,186 +28,173 @@ from requests import HTTPError
 # https://support.kraken.com/hc/en-us/articles/206548367-What-is-the-API-call-rate-limit-
 
 
-class CallRateLimitError(Exception):
-    pass
+class CallLimitError(Exception):
+    def __init__(self, msg, retry_after):
+        self.retry_after = retry_after
+        super().__init__(msg + f"Retry after: {self.retry_after}")
 
+
+def slumber(secs: float = 1.0) -> None:
+    """
+    Make sure we actually sleep enough
+    :return:
+    """
+    total_sleeptime = 0
+    start = time.perf_counter()
+    while total_sleeptime < secs:
+        time.sleep(secs - total_sleeptime)
+        total_sleeptime = time.perf_counter() - start
+
+
+# TODO : this can be made generic by parametrizing on the type float
+# TODO : optimized interval logic ?
+class PeriodBounds(typing.NamedTuple):
+    upper: typing.Optional[float] = None
+    lower: typing.Optional[float] = None
+
+
+# TODO : invertible to get FrequencyBounds...
+
+
+def check_call_period_bounds(bounds: PeriodBounds, last_period: float, on_under: typing.Optional[typing.Callable] = None, on_over: typing.Optional[typing.Callable] = None):
+    proceed = True  # proceed by default
+
+    if bounds.lower and last_period < bounds.lower:
+        try:
+            proceed = on_under(last_period, bounds.lower)
+        except TypeError:
+            if on_under is None:
+                return True  # no action on out of bounds -> keep usual semantics
+            raise
+    elif bounds.upper and last_period > bounds.upper:
+        try:
+            proceed = on_over(last_period, bounds.upper)
+        except TypeError:
+            if on_over is None:
+                return True  # no action on out of bounds -> keep usual semantics
+            raise
+    return proceed
+
+
+# TODO : numpy-based PID controller of procedure call (ie function application) period.
+
+# TODO : relate with profiler ? we need to measure time and minimize the additional time taken by measuring time...
 
 @dataclass
-class CallLimiter:
-    """
-    An immutable data structure to limit calls times.
-    Composable : limiter()()()() will keep the call frequency
-    >>> l = CallLimiter(frequency=1)
-    >>> start = time.perf_counter()
-    >>> l()
-    >>> lap = time.perf_counter() - start
-    >>> print(lap)
+class CallBoundedFunc:
 
-    >>> l()()
-    >>> lap = time.perf_counter() - lap
-    >>> print(lap)
+    # TODO : require a pure function (or idempotent, depending if you consider the state as an arg or not)
+    func: typing.Callable = field(default=lambda: None)
+    calltimer: typing.List[float] = field(init=False, default_factory=list, repr=False)  # TODO : bound in size
 
-    """
-    #: We want the current time to be able to add and remove limiter
-    #: with immediate effect (prevent call bursts)
+    limits: PeriodBounds = field(default=PeriodBounds(lower=1.0, upper=None))
 
-    target_frequency: float = 1.0
-    _now: typing.Callable = field(default=time.perf_counter, repr=False)
-    _sleep: typing.Callable = field(default=time.sleep, repr=False)
+    on_undercall: typing.Optional[typing.Callable] = None
+    on_overcall: typing.Optional[typing.Callable] = None
 
-    epsilon: float = 0.01  # error (WARNING : this will depend on your OS/machine !)
-    _periods_measured: typing.List[float] = field(init=False, default_factory=list, repr=False)
-    # Last call needs to be initialized with _now()
-    _last_call: float = field(init=False, default_factory=time.perf_counter, repr=False)
+    def __post_init__(self):
+        # TODO : access cached value for func.
+        # Design : func should be an specific class instance
+        #          that allows retrieving from cache or not depending on params or context...
+        self.last_res = None
+        self.calltimer.append(time.perf_counter())
 
-    def __call__(self):
-        #TODO : make it async proof if more is needed.
-        #TODO : make it multithread proof.... if possible.
-        period = 1 / self.target_frequency  # Hz to secs
-
-        # if we need to, we wait... (note we could also async wait...)
-        begin = self._now()
-        since_last = begin - self._last_call
-        if since_last < period:
-            self._sleep(period - since_last)
-
-        last_period = self._now() - begin
-
-        if last_period < period:  # we didn't sleep enough
-            self._sleep(self.epsilon)  # sleep a bit more
-            # double epsilon
-            self.epsilon += self.epsilon
-        elif last_period - period < self.epsilon:  # TODO : work on control theory to optimize call frequency based on measured perf
-            # slept more than enough
-            self.epsilon -= self.epsilon / 2
-
-        # last_period after epsilon rectification
-        last_period = self._now() - begin
-
-        self._periods_measured.append(last_period)
-        self._last_call = self._now()
-        return self
+    def __call__(self, *args, **kwargs):
+        # TODO : manage undercall properly, especially first time (initialization, etc.)
+        proceed = check_call_period_bounds(
+            bounds=self.limits,
+            last_period=time.perf_counter() - self.calltimer[-1],
+            on_under=self.on_overcall,  # Note over period is under frequency and vice versa...
+            on_over=self.on_undercall)
+        if proceed:
+            self.calltimer.append(time.perf_counter())
+            self.last_res = self.func(*args, **kwargs)
+        return self.last_res
 
     @property
-    def average_period(self):
-        return sum(self._periods_measured) / len(self._periods_measured)
+    def average_call_period(self):
+        if len(self.calltimer) < 2:
+            return None
+        # Hardcoded precision given most computer clock -> TODO : dynamic
+        return round(math.fsum((u-t for t, u in zip(self.calltimer, self.calltimer[1:]))) / (len(self.calltimer) -1), 4)
+
+    @property
+    def last_call_period(self):
+        if len(self.calltimer) < 2:
+            return None
+        # Hardcoded precision given most computer clock -> TODO : dynamic
+        return round(self.calltimer[-1] - self.calltimer[-2], 4)
 
 
-def limiter(limiter: CallLimiter = CallLimiter()):
+def wait_limiter(max_cps=1):
     """
-    a limiter to use as a decorator.
-    :param limiter: A limiter can be passed. useful to limit a group of functions together as one
+    A call limiter to use as a decorator.
+    This limiter will wait until it is allowed to call the function again.
+    Note : you can use the returned function to decorate multiple functions, if you want to limit them as a group.
+    :param max_cps: Maximum number of calls per seconds to enforce
     :return: a decorator to apply to the function you want to limit.
     """
 
+    def sleep_n_proceed(last_period, bound):
+        slumber(bound - last_period)
+        return True
+
     def decorator(func):
-        """
-        A decorator
-        :param func:
-        :param profiler:
-        :return:
-        """
-
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs):
-            limiter()
-            return func(*args, **kwargs)
-
-        return wrapper
+        # Todo : proper wrapper ?
+        return CallBoundedFunc(func=func, limits=PeriodBounds(lower=max_cps, upper=None), on_overcall=sleep_n_proceed)
 
     return decorator
 
 
+def drop_limiter(max_cps=1):
+    """
+    A call limiter to use as a decorator.
+    This limiter will drop the call by raising an exception if it is called too quickly.
+    Note : you can use the returned function to decorate multiple functions, if you want to limit them as a group.
+    WARNING : to not change the semantics of your program, the function should be "as pure as possible",
+              ie, skipping a call should not matter...
+    :param max_cps: Maximum number of calls per seconds to enforce
+    :return: a decorator to apply to the function you want to limit.
+    """
 
-############ from pykrakenapi
+    def drop(last_period, bound):
+        return False  # ignore arguments and return false to drop the call (and use a cached value)
 
-def crl_sleep(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
+    def decorator(func):
+        # Todo : proper wrapper ?
+        return CallBoundedFunc(func=func, limits=PeriodBounds(lower=max_cps, upper=None), on_overcall=drop)
 
-        self = args[0]
-        crl_sleep = self.crl_sleep
-
-        # raise CallRateLimitError if crl sleep is deactivated
-        if crl_sleep == 0:
-            result = func(*args, **kwargs)
-            return result
-
-        # otherwise, retry after "crl_sleep" seconds
-        while True:
-            try:
-                result = func(*args, **kwargs)
-                return result
-            except CallRateLimitError as err:
-                print(err, '\n sleeping for {} seconds'.format(crl_sleep))
-                time.sleep(crl_sleep)
-                continue
-
-    return wrapper
-
-
-def callrate_limiter(query_type):
-    def decorate_func(func):
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs):
-            """Call rate limit counter.
-
-            Implementation of a call rate limiter as a decorator. If the call
-            rate limit is reached, api calls will be blocked.
-
-            See https://support.kraken.com/hc/en-us/articles/206548367
-
-            """
-
-            self = args[0]
-
-            # determine increment
-            if query_type == 'ledger/trade history':
-                incr = 2
-            elif query_type == 'other':
-                incr = 1
-
-            # decrease api counter
-            self._decrease_api_counter()
-
-            # return api call
-            if self.api_counter < self.limit:
-                # no retries
-                if self.retry == 0:
-                    self.api_counter += incr
-                    result = func(*args, **kwargs)
-                    return result
-                # do retries
-                else:
-                    attempt = 0
-                    while self.api_counter < self.limit:
-                        try:
-                            self.api_counter += incr
-                            result = func(*args, **kwargs)
-                            return result
-                        except (HTTPError, errors.CrypyException) as err:
-                            print('attempt: {} |'.format(
-                                str(attempt).zfill(3)), err)
-                            attempt += 1
-                            time.sleep(self.retry)
-                            self._decrease_api_counter()
-                            continue
-
-            # raise error if limit exceeded
-            msg = ("call rate limiter exceeded (counter={}, limit={})")
-            msg = msg.format(str(self.api_counter).zfill(2),
-                             str(self.limit).zfill(2))
-            raise CallRateLimitError(msg)
-
-        return wrapper
-    return decorate_func
+    return decorator
 
 
 if __name__ == '__main__':
-    l = CallLimiter(target_frequency=1)
+
+    print("Checking wait limiter:")
     # tick tock the clock
     start = time.time()
-    for i in range(60):
-        l()
-        print(f"Limiter {l} last period {l._periods_measured[-1]} avg {l.average_period}")
+
+    @wait_limiter(max_cps=1)
+    def printtime():
+        print(f"{time.time()-start}")
+
+    for i in range(30):
+        printtime()
+        print(f"Limiter last period {printtime.last_call_period} avg {printtime.average_call_period}")
+
+    print("Checking drop limiter:")
+    # tick tock the clock
+    start = time.time()
+    val = 0
+
+    @drop_limiter(max_cps=1)
+    def incr():
+        global val
+        print(f"{time.time() - start}")
+        val += 1
+        return val
+
+    while time.time() - start < 30:
+        time.sleep(.3)  # avoiding cpu overload, but still faster than limiter
+        print(f"Limiter last period {printtime.last_call_period} avg {printtime.average_call_period}")
+        print(f"{incr()}")
 
