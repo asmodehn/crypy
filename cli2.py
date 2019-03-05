032e3d82c2828a1b373b9be8ebf108426bef5572
XorfacX <xorfacx@fairydwarves.com>
2019-03-05 19:15:09 +0100

    CLI2: wip to handle orders

--- cli2.py
+++ cli2.py
@@ -1,5 +1,8 @@
 import click
 
+import copy
+import types
+
 defEXCHANGE = "kraken"
 defPAIR = "ETHUSD"
 
@@ -131,17 +134,14 @@ class Desk(object):
         for pair in wholeData:
             print(f"{pair} {what}: {str(wholeData[pair][what])}")
 
-    def do_pair(self, pair="ETHUSD"):
-        pair = defPAIR if pair is '' else pair.upper()
-        #Pair(prompt_apd=pair, usedPair=pair)
-
 
 ### CLI Commands
 @click.group()
 @click.option('-e', '--exchange', default=defEXCHANGE, type=str, show_default=True) #https://click.palletsprojects.com/en/7.x/options/#choice-options
 @click.pass_context
 def cli(ctx, exchange):
-    click.echo(f"Trading on {exchange}")
+    click.echo(f"-== TRADING CLI ==-")
+    click.echo(f"EXCHANGE: {exchange}")
     ctx.obj = Desk(exchange)
 
 @cli.command()
@@ -160,28 +160,106 @@ def list(ctx, what):
 
 
 
+class Order():
+    """
+    Order class WIP
+    abstract
+    """
+    struct = {
+        'side': 'long|short',
+        'type': 'limit(default)|market|stop loss|take profit',
+        'amount': 'TBD',
+        'price': 'TBD',
+        'leverage': '1->5(default)',
+        'expiracy': 'none(default)|TBD'
+    }
+
+    def __init__(self, side):
+        self.data = self.struct   # copy
+        self.data['side'] = side  # TODO make self.data.side immutable after __init__
+        self.changes = []
+
+        def changekey(key):
+            def changeval(me, val):
+                me.changes += [(key, val)]
+                # TODO value checking
+                # TODO defaults
+            return changeval
+
+        for k in self.data.keys():
+            change_cmd = types.MethodType(changekey(k), self)
+            setattr(self, 'do_' + k, change_cmd)
+
+        #super().__init__(prompt_apd if prompt_apd is None else prompt_apd)
+
+    
+        # opening
+        print("--> format: ")
+        for k, v in self.data.items():
+            print(f"¤ {k} -> {v}")
+
+    #    super().preloop()
+
+    #def postloop(self):
+    #    try:
+    #        update = input(f"Do you want to {self.data['side'].upper()} the current pair (TODO display pair) w the following order (TODO display order info) ? [y/n]")
+    #        if update not in ['n', 'no']:
+    #            self.apply_changes(self.data)
+    #    except EOFError:
+    #        print("Everything has been cancelled, input has been closed.")
+
+    #    super().postloop()
+
+    #def apply_changes(self, mut_d):
+    #    for c in self.changes:
+    #        # apply changes
+    #        mut_d[c[0]] = c[1]
+
+    #def do_undo(self, arg):
+    #    self.changes.pop()
+
+    def do_show(self, arg):
+        c = copy.deepcopy(self.data)
+        self.apply_changes(c)
+
+        for k, v in c.items():
+            print(f"¤ {k} -> {v}")
+
+
+class Short(Order):
+    """
+    Short class
+    """
+    def __init__(self):
+        super().__init__(side="short")
+
+class Long(Order):
+    """
+    Long class
+    """
+    def __init__(self):
+        super().__init__(side="long")
 
 
 ### CLI PAIR Sub Commands
 @cli.group()
-@click.option('-t', '--ticker', default=defPAIR, type=str, show_default=True)
+@click.option('-t', '--ticker', default=defPAIR, type=str, show_default=True)  #TODO define valid pair
 @click.pass_obj
 def pair(ctx, ticker):
     """
-    Trading a specific pair. Choose one of [ETHUSD, ETHEUR]
+    Trading a specific pair defined by its ticker
     """
     click.echo(f"PAIR: {ticker}")
-    #ctx.obj = Pair(ticker)
+    ctx.ticker = ticker
 
 @pair.command()
 @click.pass_obj
 def short(ctx):
     """
-    Shorting the pair
+    Shorting a pair
     """
-    click.echo(f'shorting')
-#Desk.add_command(list)
-#Desk.add_command(pair)
+    click.echo(f'defining short order on {ctx.ticker}')
+    Short()
 
 if __name__ == '__main__':
     cli()
