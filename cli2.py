f1eebcfeda0f411891577e04ddf5021c831d4650
XorfacX <xorfacx@fairydwarves.com>
2019-03-06 12:43:56 +0100

    CLI2: short process

--- cli2.py
+++ cli2.py
@@ -13,21 +13,18 @@ wholeData = {
         'data': {
             'value': 105,
             'indicators': { 'rsi': 52.01, 'ew': 'five of five of 1 up' },
-            #### TIMEFRAME for data ?? ####
+            #### TIMEFRAME for data ??  ####
             'orderbook': []
         },
-        'positions': [
-            {
+        'positions': [{
                 'side': 'short',
                 'amount': 60,
                 'price': 92.19,
                 'leverage': 5,
                 'p/l %': 10,
                 'p/l': 800
-            }
-        ],
-        'orders': [
-            {
+            }],
+        'orders': [{
                 'side': 'short',
                 'type': 'limit',
                 'amount': 30,
@@ -41,10 +38,8 @@ wholeData = {
                 'amount': 35,
                 'price': 96,
                 'leverage': 5
-            }
-        ],
-        'trades': [
-            {
+            }],
+        'trades': [{
                 'side': 'short',
                 'type': 'limit',
                 'amount': 35,
@@ -59,28 +54,24 @@ wholeData = {
                 'price': 95,
                 'leverage': 5,
                 'datetime': '2018/09/02 15:32:12'
-            }
-        ]
+            }]
     },
     'ETHEUR': {
         'data': {
             'value': 100,
             'indicators': { 'rsi': 49.01, 'ew': 'five of five of 1 up' },
-            #### TIMEFRAME for data ?? ####
+            #### TIMEFRAME for data ??  ####
             'orderbook': []
         },
-        'positions': [
-            {
+        'positions': [{
                 'side': 'short',
                 'amount': 60,
                 'price': 94.19,
                 'leverage': 5,
                 'p/l %': 10,
                 'p/l': 800
-            }
-        ],
-        'orders': [
-            {
+            }],
+        'orders': [{
                 'side': 'short',
                 'type': 'limit',
                 'amount': 30,
@@ -94,10 +85,8 @@ wholeData = {
                 'amount': 34,
                 'price': 76,
                 'leverage': 10
-            }
-        ],
-        'trades': [
-            {
+            }],
+        'trades': [{
                 'side': 'short',
                 'type': 'limit',
                 'amount': 35,
@@ -112,8 +101,7 @@ wholeData = {
                 'price': 89,
                 'leverage': 5,
                 'datetime': '2018/09/02 15:34:12'
-            }
-        ]
+            }]
     }
 }
 
@@ -174,76 +162,28 @@ class Order():
         'expiracy': 'none(default)|TBD'
     }
 
-    def __init__(self, side):
+    def __init__(self, side, ticker, order_type, leverage, expiracy, amount, price):
         self.data = self.struct   # copy
         self.data['side'] = side  # TODO make self.data.side immutable after __init__
-        self.changes = []
+        self.data['type'] = order_type 
+        self.data['leverage'] = leverage 
+        self.data['expiracy'] = expiracy 
+        self.data['amount'] = amount 
+        self.data['price'] = price 
+        #TODO use TICKER
 
-        def changekey(key):
-            def changeval(me, val):
-                me.changes += [(key, val)]
-                # TODO value checking
-                # TODO defaults
-            return changeval
-
-        for k in self.data.keys():
-            change_cmd = types.MethodType(changekey(k), self)
-            setattr(self, 'do_' + k, change_cmd)
-
-        #super().__init__(prompt_apd if prompt_apd is None else prompt_apd)
-
-    
-        # opening
-        print("--> format: ")
+    def showData(self):
         for k, v in self.data.items():
             print(f"¤ {k} -> {v}")
 
-    #    super().preloop()
-
-    #def postloop(self):
-    #    try:
-    #        update = input(f"Do you want to {self.data['side'].upper()} the current pair (TODO display pair) w the following order (TODO display order info) ? [y/n]")
-    #        if update not in ['n', 'no']:
-    #            self.apply_changes(self.data)
-    #    except EOFError:
-    #        print("Everything has been cancelled, input has been closed.")
-
-    #    super().postloop()
-
-    #def apply_changes(self, mut_d):
-    #    for c in self.changes:
-    #        # apply changes
-    #        mut_d[c[0]] = c[1]
-
-    #def do_undo(self, arg):
-    #    self.changes.pop()
-
-    def do_show(self, arg):
-        c = copy.deepcopy(self.data)
-        self.apply_changes(c)
-
-        for k, v in c.items():
-            print(f"¤ {k} -> {v}")
-
-
-class Short(Order):
-    """
-    Short class
-    """
-    def __init__(self):
-        super().__init__(side="short")
-
-class Long(Order):
-    """
-    Long class
-    """
-    def __init__(self):
-        super().__init__(side="long")
+    def execute(self):
+        #TODO pass order to wholeData
+        pass
 
 
 ### CLI PAIR Sub Commands
 @cli.group()
-@click.option('-t', '--ticker', default=defPAIR, type=str, show_default=True)  #TODO define valid pair
+@click.option('-t', '--ticker', default=defPAIR, type=str, show_default=True)  #TODO define valid pair tickers per exchange
 @click.pass_obj
 def pair(ctx, ticker):
     """
@@ -253,17 +193,23 @@ def pair(ctx, ticker):
     ctx.ticker = ticker
 
 @pair.command()
+@click.option('-ot', '--order-type', default='limit', type=click.Choice(['limit', 'market', 'stop loss', 'take profit']), show_default=True)
+@click.option('-lv', '--leverage', type=click.IntRange(1, 5), default=1, show_default=True)
+@click.option('-exp', '--expiracy', type=str, default='none', show_default=True) #TODO use it #TODO handle datetime format #(https://click.palletsprojects.com/en/7.x/options/#callbacks-for-validation)
+@click.argument('amount_price', nargs=2, type=float)
+#@click.confirmation_option(prompt='Short?')
 @click.pass_obj
-def short(ctx):
+def short(ctx, order_type, leverage, expiracy, amount_price):
     """
     Shorting a pair
     """
-    click.echo(f'defining short order on {ctx.ticker}')
-    Short()
+    click.echo(f'Do you want to execute the following short on {ctx.ticker} ?')
+    order = Order(ticker = ctx.ticker, side="short", order_type = order_type, leverage = leverage, expiracy = expiracy, amount=amount_price[0], price=amount_price[1])
+    order.showData()
+
+    click.confirm('Please confirm', abort=True) #die here if No is selected (default) otherwise continue code below
+    order.execute()
+
 
 if __name__ == '__main__':
     cli()
-
-
-#Example USAGE ATM:
-#python cli2.py --exchange %exchange_name% list [data|orders|positions|trades]
