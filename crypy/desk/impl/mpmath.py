2b4f056d4c3de0ff049546b3a81a6a5a7f50a457
AlexV <asmodehn@gmail.com>
2019-03-09 14:07:10 +0100

    adding an interface to mpmath, with tests to verify behavior.

---
+++ crypy/desk/impl/mpmath.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+import typing
+import hypothesis
+import hypothesis.strategies
+
+# Ref : https://en.wikipedia.org/wiki/Interval_arithmetic
+
+# Ref : http://mpmath.org/
+
+try:
+    from ...euc import mpmath
+    from ..errors import CrypyException
+except (ImportError, ValueError):
+    from crypy.euc import mpmath
+    from crypy.desk.errors import CrypyException
+
+
+"""
+Module providing an interface to mpmath, allowing it to work with pydantic for runtime type enforcement.
+Note the dataclasses here are pure python dataclasses.
+pydantic is not involved in the type enforcement at this level.
+"""
+
+
+class MPFloat(mpmath.mpf):
+    """
+    A Basic Multiprecision float (in the mpmath sense)
+    """
+    @staticmethod
+    def strategy(min_value: float=None, max_value: float=None, exclude_min: bool=False, exclude_max=False, allow_infinity=False):
+        return hypothesis.strategies.builds(MPFloat, hypothesis.strategies.floats(
+            allow_nan=False,
+            min_value=min_value,
+            max_value=max_value,
+            exclude_min=exclude_min,
+            exclude_max=exclude_max,
+            allow_infinity=allow_infinity,  # careful https://github.com/HypothesisWorks/hypothesis/issues/1859
+        ))
+
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        return mpmath.mpf(v)
+
+
+class MPFuzzyFloat(mpmath.mpf):
+    """
+    A Fuzzy Float, ie, a float that is somewhere in an interval,
+    and this interval is managed during arithmetic computation.
+    """
+    @staticmethod
+    def strategy(min_value: float=None, max_value: float=None, exclude_min: bool=False, exclude_max=False, allow_infinity=False):
+        return hypothesis.strategies.builds(MPFloat, hypothesis.strategies.floats(
+            allow_nan=False,
+            min_value=min_value,
+            max_value=max_value,
+            exclude_min=exclude_min,
+            exclude_max=exclude_max,
+            allow_infinity=allow_infinity,  # careful https://github.com/HypothesisWorks/hypothesis/issues/1859
+        ))
+
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v: float):
+        return mpmath.iv.mpf(str(v))
+
+
+class MPInterval(mpmath.iv.mpf):
+    """
+    An interval on the float line.
+    """
+
+    @staticmethod
+    @hypothesis.strategies.composite
+    def strategy(draw):
+        low_bound = draw(hypothesis.strategies.floats(allow_nan=False))
+        high_bound = draw(hypothesis.strategies.floats(allow_nan=False, min_value=low_bound))
+        return MPInterval((low_bound, high_bound))
+
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, b: typing.Tuple[float, float]):
+        """enforce needing a pair to define an interval"""
+        assert b[1] >= b[0]  # TODO : can interval be inverted (inverting the meaning of being "in" to "out") ??
+        return mpmath.mpi(b[0], b[1])
+
---
+++ crypy/desk/impl/tests/test_mpmath.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+import pytest
+import typing
+from hypothesis import given, Verbosity, settings
+import hypothesis.strategies
+
+from ..mpmath import MPFloat, MPFuzzyFloat, MPInterval
+
+"""
+To Verify MPMath Semantics and wrap it in types that makes sense for us
+"""
+
+
+@given(mpf=MPFloat.strategy())
+@settings(verbosity=Verbosity.verbose)
+def test_mpfloat_identity_equality(mpf):
+
+    assert mpf is not float(mpf)
+
+    mpf_bis = mpf
+
+    assert mpf_bis is mpf
+    assert mpf_bis == mpf
+
+    mpg = MPFloat(float(mpf))
+
+    assert mpg is not mpf
+    assert mpg == mpf
+
+
+@given(mpff=MPFuzzyFloat.strategy())
+@settings(verbosity=Verbosity.verbose)
+def test_mpfuzzyfloat_identity_equality(mpff):
+
+    assert mpff is not float(mpff)
+
+    mpff_bis = mpff
+
+    assert mpff_bis is mpff
+    assert mpff_bis == mpff
+
+    mpfg = MPFuzzyFloat(float(mpff))
+
+    assert mpfg is not mpff
+    assert mpfg == mpff
+
+
+@given(mpij=MPInterval.strategy())
+@settings(verbosity=Verbosity.verbose)
+def test_mpinterval_identity_equality(mpij):
+
+    mpij_bis = mpij
+
+    assert mpij_bis is mpij
+    assert mpij_bis == mpij
+
+    mpjk = MPInterval((mpij.a, mpij.b))
+
+    assert mpjk is not mpij
+    assert mpjk == mpij
+
+
+@given(mpf=MPFloat.strategy(), val=hypothesis.strategies.floats(allow_nan=False, allow_infinity=False))
+@settings(verbosity=Verbosity.verbose)
+def test_mpfloat_add_sub(mpf, val):
+
+    added_raw = mpf + val
+    added_mp = mpf + MPFloat(val)
+
+    assert added_raw == added_mp
+
+    sub_raw_raw = added_raw - val
+
+    sub_mp_raw = added_mp - val
+
+    sub_raw_mp = added_raw - MPFloat(val)
+
+    sub_mp_mp = added_mp - MPFloat(val)
+
+    assert sub_raw_raw == sub_mp_raw == sub_raw_mp == sub_mp_mp
+
+
+@given(mpf=MPFuzzyFloat.strategy(), val=hypothesis.strategies.floats(allow_nan=False, allow_infinity=False))
+@settings(verbosity=Verbosity.verbose)
+def test_mpfuzzyfloat_add_sub(mpf, val):
+
+    added_raw = mpf + val
+    added_fuzz = mpf + MPFuzzyFloat(val)
+
+    assert added_raw == added_fuzz
+
+    sub_raw_raw = added_raw - val
+
+    sub_mp_raw = added_fuzz - val
+
+    sub_raw_mp = added_raw - MPFuzzyFloat(val)
+
+    sub_mp_mp = added_fuzz - MPFuzzyFloat(val)
+
+    assert sub_raw_raw == sub_mp_raw == sub_raw_mp == sub_mp_mp
+
+
+@given(mpia=MPInterval.strategy(), mpib=MPInterval.strategy(),)
+@settings(verbosity=Verbosity.verbose)
+def test_mpinterval_add_sub(mpia, mpib):
+
+    added = mpia + mpib
+
+    subbeda = added - mpia
+
+    assert mpib in subbeda
+
+    subbedb = added - mpib
+
+    assert mpia in subbedb
+
+
+if __name__ == "__main__":
+    pytest.main(["-s", __file__])
