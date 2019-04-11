60f9ac58698f8326e8506a41cee1ce8836882f8a
AlexV <asmodehn@gmail.com>
2019-04-11 15:01:43 +0200

    removed useless utils class

--- crypy/desk/utils.py
+++ crypy/desk/utils.py
@@ -6,15 +6,13 @@ import sys
 import json
 import datetime
 
-class Utils:
-    def formatTS(msts):
-        """Format a UNIX timestamp in millesecond to truncated ISO8601 format"""
-        #TODO look into import arrow https://github.com/crsmithdev/arrow
-        return datetime.datetime.utcfromtimestamp(msts/1000).strftime("%Y-%m-%d %H:%M:%S")
+def formatTS(msts):
+    """Format a UNIX timestamp in millesecond to truncated ISO8601 format"""
+    #TODO look into import arrow https://github.com/crsmithdev/arrow
+    return datetime.datetime.utcfromtimestamp(msts/1000).strftime("%Y-%m-%d %H:%M:%S")
 
 
-    def ppJSON(res):
-        """Pretty-print a JSON result"""
-        if res is not None:
-            print(json.dumps(res, indent=2))
-    
+def ppJSON(res):
+    """Pretty-print a JSON result"""
+    if res is not None:
+        print(json.dumps(res, indent=2))
