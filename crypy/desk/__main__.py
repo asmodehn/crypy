66177ad94a42e5ef396d9f9340fc27c0ebade695
XorfacX <xorfacx@fairydwarves.com>
2019-04-16 12:10:45 +0200

    cli order to its own file

--- crypy/desk/__main__.py
+++ crypy/desk/__main__.py
@@ -3,7 +3,11 @@
 
 from cli import cli
 from capital_cli import capital
+from order_cli import order_group
 
+"""Entrypoint for the desk subpackage
+Manages one (currently) exchange, via CLI
+"""
 
 if __name__ == '__main__':
     #try:
