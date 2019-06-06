950d40a676b57222852a653b71a06609b17eaf6f
AlexV <asmodehn@gmail.com>
2019-06-06 12:28:49 +0200

    exploring an alternative route with marshmallow

--- crypy/desk_new/commands/sample.py
+++ crypy/desk_new/commands/sample.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import datetime
 import functools
 import types
 
@@ -12,175 +13,66 @@ import unittest
 import prompt_toolkit
 from prompt_toolkit.patch_stdout import patch_stdout
 
+import datetime as dt
+from dataclasses import dataclass
 
-def prompt_patch(instance: BaseModel):
-    """
-    Patching pydantic because extending BaseModel seems to break the debugger currently (0.27)
-    :param model:
-    :return:
-    """
+from marshmallow import Schema, fields, pprint
 
-    def process_values(self, input_data: typing.Any) -> typing.Dict[str, typing.Any]:
-        # (casting here is slow so use ignore)
-        values, errors = validate_model(self, input_data, raise_exc=False)  # type: ignore
+# MODELS
 
-        # CAREFUL : breakpoints here will break the debugger !!! (pydantic 0.27)
-
-        if errors:
-            if self.prompts:
-                for e in errors.raw_errors:
-                    print(e.msg)
-                    f = getattr(self, e['loc'][0])
-                    if self.prompts:
-                        try:
-                            f.type_.prompt(data=input_data.get('data'))
-                        except Exception as e:
-                            print(e)
-                            raise e
-                            # TODO : handle properly?
-            else:
-                raise errors
-        else:
-            return values
-
-    def prompt(cls, data= None, again=False):  # data can be the previous prompt or default value...
-        a = None
-        with patch_stdout():
-            data = prompt_toolkit.prompt(
-                message=f"{cls.__name__}: ",
-                async_=True,
-                # TODO : prefill with default ? auto complete with previous ?
-            )
-            while a is None:
-                if again:  # avoiding too deep
-                    try:
-                        a = cls.create(data_dict=data)
-                    except ValidationError as ve:
-                        # creation failed, loop instead of recursing in fixer_cb
-                        a = None  # guarantee that we will loop
-                else:
-                    a = cls(prompts=False, **data)
-
-        return a
-
-    # Patching
-    instance._process_values = types.MethodType(process_values, instance)  # instance method
-    instance.prompts = prompt  # class method
-
-    return instance
-
-#
-# class PromptBaseModel(BaseModel):
-#
-#     """
-#     Extending Pydantic Base Model with a prompt function.
-#     """
-#     prompts: bool  # TODO : enrich with a prompt function / schema/ structure/ strategy, whatever you wanna call it
-#
-#     def __init__(self, prompts: bool = False, **data: typing.Any) -> None:
-#         # CAREFUL : breakpoints here will break the debugger !!! (pydantic 0.27)
-#         self.prompts = prompts
-#         super().__init__(**data)
-#
-#
-#     def _process_values(self, input_data: typing.Any) -> typing.Dict[str, typing.Any]:
-#         # (casting here is slow so use ignore)
-#         values, errors = validate_model(self, input_data, raise_exc=False)  # type: ignore
-#
-#         # CAREFUL : breakpoints here will break the debugger !!! (pydantic 0.27)
-#
-#         if errors:
-#             if self.prompts:
-#                 for e in errors.raw_errors:
-#                     print(e.msg)
-#                     f = self.get(e['loc'][0])
-#                     if self.prompts:
-#                         try:
-#                             f.type_.prompt(data=input_data.get('data'))
-#                         except Exception as e:
-#                             print(e)
-#                             raise e
-#                             # TODO : handle properly?
-#             else:
-#                 raise errors
-#         else:
-#             return values
-#
-#     @classmethod
-#     def prompt(cls, data= None, again=False):  # data can be the previous prompt or default value...
-#         a = None
-#         with patch_stdout():
-#             data = prompt_toolkit.prompt(
-#                 message=f"{cls.__name__}: ",
-#                 async_=True,
-#                 # TODO : prefill with default ? auto complete with previous ?
-#             )
-#             while a is None:
-#                 if again:  # avoiding too deep
-#                     try:
-#                         a = cls.create(data_dict=data)
-#                     except ValidationError as ve:
-#                         # creation failed, loop instead of recursing in fixer_cb
-#                         a = None  # guarantee that we will loop
-#                 else:
-#                     a = cls(prompts=False, **data)
-#
-#         return a
+@dataclass
+class Email:
+    user: str  # HOW TO enforce string content via types
+    hostname: str  # HOWTO enforce string content via types ?
 
 
-class Answer(BaseModel):
+@dataclass(init=False)
+class User:
+    name: str
+    email: Email
+    created_at: datetime.datetime
 
-    data: int
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.created_at = dt.datetime.now()
 
-    # Careful when playing with __init__, BaseModel.__getattr__ will break and so will the debugger
-
-    def printme(self):
-        print(self.data)
-        # Complicated code will be here
-        return True
-
-
-Answer = prompt_patch(Answer)
-
-
-class Nested(BaseModel):
-
-    answer: Answer
-
-    # Careful when playing with __init__, BaseModel.__getattr__ will break and so will the debugger
-
-    def printme(self):
-        print({
-            'data': self.data
-        })
-        # Complicated code will be here
-        return True
-
-Nested = prompt_patch(Nested)
+    def __repr__(self):
+        return '<User(name={self.name!r})>'.format(self=self)
 
 
-if __name__ == '__main__':
+# SCHEMAS ( how to convert to / from strings )
+# Not a type check but a parsing validation ( not as strict, but probably more practical in python)
 
 
-    # should work
-    a1 = Answer(**{"data": "42"})
+class UserSchema(Schema):
+    name = fields.Str()
+    email = fields.Email()
+    created_at = fields.DateTime()
 
-    assert a1.data == 42
 
-    # that too
-    a2 = Answer(data="42")
-    assert a2.data == 42
+user = User(name="Monty", email="monty@python.org")
+schema = UserSchema()
+result = schema.dump(user)
+pprint(result)
+# {"name": "Monty",
+#  "email": "monty@python.org",
+#  "created_at": "2014-08-17T14:54:16.049594+00:00"}
 
-    # or even that
-    a3 = Answer(data=42)
 
-    assert a3.data == 42
+user_data = {
+    'created_at': '2014-08-11T05:26:03.869245',
+    'email': u'ken@yahoo.com',
+    'name': u'Ken'
+}
+schema = UserSchema()
+result = schema.load(user_data)
+pprint(result)
+# {'name': 'Ken',
+#  'email': 'ken@yahoo.com',
+#  'created_at': datetime.datetime(2014, 8, 11, 5, 26, 3, 869245)},
 
-    # other ways will NOT
-    # Answer(42)
-    # Answer("42")
-    # Answer({"data": "42"})
-    # etc.
-
-    # should prompt and loop until user gets it right
-    Answer(data="bob")
+# TODO : plug tests (hypothesis) on that
+# TODO : plug click on that
+# TODO : plug repl prompt on that
+# Note plugging json load/dump for neetwork already managed by marshmallow
